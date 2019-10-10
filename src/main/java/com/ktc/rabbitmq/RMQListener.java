e7eb7917fa545d718540d825a8d3c34a2b690752
Svyatoslav Nadozirny <nadozirny.s@gmail.com>
2019-10-10 05:59:11 +0000

    test added

--- src/main/java/com/ktc/rabbitmq/RMQListener.java
+++ src/main/java/com/ktc/rabbitmq/RMQListener.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
 import com.rabbitmq.client.Delivery;
-
+import com.rabbitmq.client.*;
 import org.camunda.bpm.application.PostDeploy;
 import org.camunda.bpm.application.PreUndeploy;
 import org.camunda.bpm.application.ProcessApplication;
@@ -14,6 +14,7 @@ import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.variable.Variables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.Map;
 
 
 @ProcessApplication(name="RMQ Listener service")
@@ -30,12 +31,39 @@ public class RMQListener extends ServletProcessApplication {
     notificationService.registerHandler(new OnDelivery() {
 	public void onDelivery(Delivery delivery){
 	    try{
-	    String message=new String(delivery.getBody(),"UTF-8");
-	
-    	    runtimeService.startProcessInstanceByKey("Process_Name",
-        	Variables.createVariables()
-        	.putValue("message", message)
-	    );
+	    AMQP.BasicProperties properties=delivery.getProperties();
+	    String type="text/plain";
+	    String message="";
+	    if (properties.getContentType()!=null){
+		type=properties.getContentType();
+	    }
+	    if (type=="text/plain" || type=="application/json" || type=="text/html"){
+		if (properties.getContentEncoding()==null || properties.getContentEncoding().isEmpty()){
+		    message=new String(delivery.getBody(),"UTF-8");
+		}else{
+		    message=new String(delivery.getBody(),properties.getContentEncoding());
+		}
+	    }
+	    Map<String,Object> headers=properties.getHeaders();
+	    String process_name=(String)headers.get("process_name");
+	    String message_name=(String)headers.get("message_name");
+	    String business_key=(String)headers.get("business_key");
+	    Map<String,Object> vars=Variables.createVariables()
+		.putValue("contentType",properties.getContentType())
+		.putValue("contentEncoding",properties.getContentEncoding())
+		.putValue("appId",properties.getAppId())
+		.putValue("messageId",properties.getMessageId())
+		.putValue("correlationId",properties.getCorrelationId())
+		.putValue("type",properties.getType())
+		.putValue("replyTo",properties.getReplyTo())
+        	.putValue("message", message);
+
+	    if (process_name!=null){
+    		runtimeService.startProcessInstanceByKey(process_name,business_key,vars);
+	    }
+	    if (message_name!=null){
+    		runtimeService.correlateMessage(message_name,business_key,vars);
+	    }
 
 	    //runtimeService:
 	    //.correlateMessage(String messageName, String businessKey, Map<String,Object> processVariables);
--- src/test/java/com/ktc/rabbitmq/AppTest.java
+++ src/test/java/com/ktc/rabbitmq/AppTest.java
@@ -1,20 +1,68 @@
 package com.ktc.rabbitmq;
 
 import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.io.IOException;
+import java.util.List;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.runtime.JobQuery;
+import org.camunda.bpm.engine.task.Task;
+import org.camunda.bpm.engine.task.TaskQuery;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+
+  @Rule
+  public ProcessEngineRule engineRule = new ProcessEngineRule();
+
+  private RMQListener processApplication;
+  private RuntimeService runtimeService;
+  private TaskService taskService;
+
+  @Before
+  public void init() throws IOException {
+    runtimeService = engineRule.getRuntimeService();
+    taskService = engineRule.getTaskService();
+
+    processApplication = new RMQListener();
+  }
+
+  @Deployment(resources = "processes/Process.bpmn")
+
+   @Test
+  public void processPrintMail() throws Exception {
+
+    processApplication.startService(engineRule.getProcessEngine());
+
+    TaskQuery taskQuery = taskService.createTaskQuery().taskName("print it");
+
+    // wait for first mail
+    while(taskQuery.count() == 0) {
+      Thread.sleep(500);
     }
+
+    List<Task> tasks = taskQuery.list();
+    assertThat(tasks).isNotEmpty();
+
+    for (Task task : tasks) {
+      taskService.complete(task.getId());
+    }
+    assertThat(runtimeService.createProcessInstanceQuery().list()).isEmpty();
+  }
+
+
+ @After
+  public void cleanup() throws Exception {
+    processApplication.stopService();
+  }
+
 }
